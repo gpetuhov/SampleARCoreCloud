f100fbe0810db06b74525c09d321bbc3371dacd1
Gennady Petukhov <gpetukhov@phoenixit.ru>
2019-03-19 14:36:47 +0300

    Show model on tap

--- app/src/main/java/com/gpetuhov/android/samplearcorecloud/MainActivity.kt
+++ app/src/main/java/com/gpetuhov/android/samplearcorecloud/MainActivity.kt
@@ -3,9 +3,17 @@ package com.gpetuhov.android.samplearcorecloud
 import android.app.Activity
 import android.app.ActivityManager
 import android.content.Context
+import android.net.Uri
 import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.MotionEvent
+import com.google.ar.core.HitResult
+import com.google.ar.core.Plane
+import com.google.ar.sceneform.AnchorNode
+import com.google.ar.sceneform.rendering.ModelRenderable
+import com.google.ar.sceneform.ux.ArFragment
+import com.google.ar.sceneform.ux.TransformableNode
 import com.pawegio.kandroid.toast
 
 class MainActivity : AppCompatActivity() {
@@ -13,6 +21,9 @@ class MainActivity : AppCompatActivity() {
     companion object {
         const val MIN_OPENGL_VERSION = 3.0
     }
+
+    private var arFragment: ArFragment? = null
+    private var modelRenderable: ModelRenderable? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -22,6 +33,11 @@ class MainActivity : AppCompatActivity() {
         }
 
         setContentView(R.layout.activity_main)
+
+        arFragment = supportFragmentManager.findFragmentById(R.id.arFragment) as ArFragment
+
+        loadModel()
+        initArFragment()
     }
 
     /**
@@ -50,5 +66,37 @@ class MainActivity : AppCompatActivity() {
         }
 
         return true
+    }
+
+    private fun loadModel() {
+        ModelRenderable.builder()
+            .setSource(this, Uri.parse("file:///android_asset/model.sfb"))
+            .build()
+            .thenAccept { renderable -> modelRenderable = renderable }
+            .exceptionally { throwable ->
+                toast("Unable to load renderable")
+                null
+            }
+    }
+
+    private fun initArFragment() {
+        arFragment?.setOnTapArPlaneListener(::onPlaneTap)
+    }
+
+    private fun onPlaneTap(hitResult: HitResult, plane: Plane, motionEvent: MotionEvent) {
+        if (modelRenderable == null) {
+            return
+        }
+
+        // Create the Anchor at the place of the tap.
+        val anchor = hitResult.createAnchor()
+        val anchorNode = AnchorNode(anchor)
+        anchorNode.setParent(arFragment?.arSceneView?.scene)
+
+        // Create the transformable model and add it to the anchor.
+        val model = TransformableNode(arFragment?.transformationSystem)
+        model.setParent(anchorNode)
+        model.renderable = modelRenderable
+        model.select()
     }
 }
